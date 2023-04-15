2820fea2ef4debcfc782deafc65762f62adb4ff4
Aruzhan Sabyrbek <74536857+IwannabeSplendid@users.noreply.github.com>
2023-04-15 19:07:20 +0600

    views

--- website/views.py
+++ website/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponseRedirect, JsonResponse
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 import requests
 
 # Home page view extends the index.html template
@@ -23,14 +24,31 @@ def home(request):
             
         order = Order.objects.get(order_id = order_number)
         print(order.client.IIN)
-        if order.client.IIN == Client.objects.get(IIN=iin):
-            return JsonResponse({'Message': 'Ok'})#render(request, 'order.html', {'order': order})
+        if order.client.IIN == iin:
+            return render(request, 'order.html', {'order_id': order.order_id})
             
         return JsonResponse(client_info)
     
     return render(request, 'home.html')
 
-
+def construct_order(request, iin):
+    if request.method == "POST":
+        iin = request.POST['iin']
+        order_number = request.POST['order_number']
+        client_info = get_client_info(iin)
+        phone = get_phone_number(iin)
+        if not Client.objects.filter(IIN=iin).exists():
+            client = Client(first_name = client_info['firstName'], last_name = client_info['lastName'], IIN = client_info['iin'], phone_number = phone)
+            client.save()
+        
+        order = Order.objects.get(order_id = order_number)
+        print(order.client.IIN)
+        if order.client.IIN == iin:
+            return render(request, 'order.html', {'order': order})
+            
+        return JsonResponse(client_info)
+    
+    return render(request, 'order.html')
 
 def get_token():
     url = 'http://hakaton-idp.gov4c.kz/auth/realms/con-web/protocol/openid-connect/token'
@@ -45,21 +63,25 @@ def login(request):
     #when submitted (button clicked)
     if request.method == "POST":
         #authenticate user
-        iin = request.POST['iin']
+        username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, iin = iin, password = password)
+        user = authenticate(request, username = username, password = password)
         
         #redirect to their pages
         if user is not None: 
-            login(request, user)
-
-            return HttpResponseRedirect(reverse('personal'))
+            auth_login(request, user)
+            return HttpResponseRedirect(reverse('home'))
         else:
             return render(request, 'users/login.html', {
                 'message': 'Invalid credentials'
             })
+            
     return render(request, 'login.html')
             
+def logout(request):
+    auth_logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
 def send_sms(phone_number, message):
     token = get_token()
     url = 'http://hak-sms123.gov4c.kz/api/smsgateway/send'
