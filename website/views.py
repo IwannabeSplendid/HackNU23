5f850175b4dc7a2ce8671ca89a7dd6879cb04f6c
Aruzhan Sabyrbek <74536857+IwannabeSplendid@users.noreply.github.com>
2023-04-15 15:18:16 +0600

    views

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import User, Courier, Employee, Client, Order, Address, Company, Department
+
 
 # Home page view extends the index.html template
 # track, login buttons 
